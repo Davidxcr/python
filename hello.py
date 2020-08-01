df42070a8cc36e05e0b5e26bf66907c1dbbc7e8b
xcript1 <o_olatunji@mail.harper.edu>
2020-08-01 18:17:05 -0500

    commit

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
-def hi():
-    print("Hello World!")
-
-
-hi()
+def runningSum(self, nums):
+    ans = [1, 2, 3]
+    tmp = 0
+    for i in nums:
+        tmp += 1
+        ans.append(tmp)
+    return ans 
