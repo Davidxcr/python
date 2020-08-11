8cb39ab0547858e4400a973ce45bbeb126cd2130
xcript1 <o_olatunji@mail.harper.edu>
2020-08-11 18:36:26 -0500

    commit

--- Weather_Report.py
+++ Weather_Report.py
@@ -12,7 +12,6 @@ items = week.find_all(class_='tombstone-container')
 period_names = [item.find(class_='period-name').get_text() for item in items]
 short_descriptions = [item.find(class_='short-desc').get_text() for item in items]
 temperatures = [item.find(class_='temp').get_text() for item in items]
-
 weather_stuff = pd.DataFrame(
     {'Period': period_names,
     'Short_descriptions': short_descriptions,
