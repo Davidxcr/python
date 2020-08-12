197a6ba29c3c69098b7064ba7d6438b25e4945d8
xcript1 <o_olatunji@mail.harper.edu>
2020-08-12 16:23:50 -0500

    commit

--- Weather_Report.py
+++ Weather_Report.py
@@ -3,12 +3,11 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
+
 page = requests.get('https://forecast.weather.gov/MapClick.php?lat=42.004470000000026&lon=-88.10280999999998#.XzMLghNKg1I')
 soup = BeautifulSoup(page.content, 'html.parser')
 week = soup.find(id='seven-day-forecast-body')
-
 items = week.find_all(class_='tombstone-container')
-
 period_names = [item.find(class_='period-name').get_text() for item in items]
 short_descriptions = [item.find(class_='short-desc').get_text() for item in items]
 temperatures = [item.find(class_='temp').get_text() for item in items]
@@ -18,6 +17,8 @@ weather_stuff = pd.DataFrame(
     'Temperature': temperatures,
     })
 
+
 print(weather_stuff)
+
 
 weather_stuff.to_csv('weather_report.csv')
