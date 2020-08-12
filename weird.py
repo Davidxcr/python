d5e5fd7c462af71d9f3ecfbc472be7d9c8e81d5a
xcript1 <o_olatunji@mail.harper.edu>
2020-08-12 18:28:26 -0500

    complete

--- weird.py
+++ weird.py
@@ -7,3 +7,17 @@ import sys
 
 if __name__ == '__main__':
     n = int(input("Please enter an integer: ").strip())
+
+
+if n % 2 == 0 and n in range (2, 6):
+    print("Not Weird") 
+
+elif n % 2 == 0 and n in range (6, 21):
+    print("Weird")
+
+elif n % 2 == 0 and n > 20:
+    print("Not Weird")
+
+else:
+    print("Weird")
+
