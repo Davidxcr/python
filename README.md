3c25bcbd6da75582ccd5400fbda217db4cad9614
xcript1 <o_olatunji@mail.harper.edu>
2020-08-15 17:15:23 -0500

    new commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # python
+
+Hi, Welcome to my Python Github Repo, Here are all the little projects i have worked on in the process of learning python
---
+++ arithmatic op.py
@@ -0,0 +1,10 @@
+class Person:
+    def __init__ (self, name,):
+        self.name = name
+        self.talk = talk
+        
+        
+        
+David = Person.name
+
+print(Person.name)
